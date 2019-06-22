addac1e3b5395c3017666de62abc5c6d2bd0639e
unknown <hannahuseva@gmail.com>
2019-06-22 03:06:58 -0500

    All done. Needs a video.

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # constructor_word_guess
 UW-Extension Boot Camp Homework Week 11
+Purpose: Learn to work with Node.js, npm packages (particularly inquirer), constructors.
+
+Description: Command line app, offers you to guess a name of cryptocreature letter by letter. 
+If you wish to clone and run it, you will have to run 'npm install' to get fs and inquirer packages. Then run node index.js in your terminal. Follow the prompt. 
+Once you guessed (or not) you have an option to guess another one or quit.
+
+*If you win
+![won](screenshots/won.png)
+
+*If you lose
+![Lost](screenshots/lost.png)
+
+See it alive:
--- index.js
+++ index.js
@@ -2,70 +2,109 @@ require("fs")
 var Word = require("./word")
 var inquirer = require("inquirer")
 
-var topic = ["kraken", "bigfoot","chupacabra","dragon","popobawa","unicorn","sphinx","amarok","irkuiem", "elwedritsch", "sirin", "mavka"]
-var chosenword = topic[Math.floor(Math.random() * topic.length)]
-var word = new Word(chosenword)
-var guesses = 15
-word.wordtoguess()
-inquirer.prompt([
-    {
-        type: "input",
-        name: "usercharacter",
-        message: "Guess this cryptocreature: "+word.showstring().join(" ") + "\nNumber of guesses: "+guesses+ "\nYour letter: ",
-        validate: function(value) {
-            if(value.match(/^[A-Za-z]+$/)&&value.length==1){
-                return true
-            }
-            else {
-                return "Please type a letter!"
-            }
-        }
-    }
-]).then(function (answers) {
-    var usercharacter = answers.usercharacter
-    word.showcharacter(usercharacter)
-    if(chosenword.indexOf(usercharacter)==-1){
-        guesses=guesses-1
-    }
-    guessprompt()
-})
+var topic = ["kraken","bigfoot","chupacabra","dragon","popobawa","unicorn","sphinx","amarok","irkuiem","elwedritsch", "sirin","mavka"]
 
-function guessprompt(){
-    if(word.showstring().join("")===chosenword){
-        console.log(chosenword.toUpperCase() + "\nVictory!!!")
-        // inquirer.prompt([
-        //     {
-
-        //     }
-    }
-    else if(guesses>0){
-        inquirer.prompt([
+function newgame () {
+    var chosenword = topic[Math.floor(Math.random() * topic.length)]
+    var word = new Word(chosenword)
+    var guesses = 15
+    var triedletters = []
+    word.wordtoguess()
+    inquirer.prompt([
         {
-        type: "input",
-        name: "usercharacter",
-        message: "Keep guessing: "+word.showstring().join(" ") + "\nNumber of guesses: "+guesses+ "\nYour letter: ",
-        validate: function(value) {
-            if(word.showstring().indexOf(value)>-1){
-                return "You already have this letter. Don't repeat!"
-            }
-            else if(value.match(/^[A-Za-z]+$/)&&value.length==1){
-                return true
-            }
-            else {
-                return "Please type a letter!"
+            type: "input",
+            name: "usercharacter",
+            message: "Guess this cryptocreature: "+word.showstring().join(" ") + "\nNumber of guesses: "+guesses+ "\nYour letter: ",
+            validate: function(value) {
+                if(value.match(/^[A-Za-z]+$/)&&value.length==1){
+                    return true
+                }
+                else {
+                    return "Please type a letter!"
+                }
             }
         }
+    ]).then(function (answers) {
+        var usercharacter = answers.usercharacter
+        triedletters.push(usercharacter)
+        word.showcharacter(usercharacter)
+        if(chosenword.indexOf(usercharacter)==-1){
+            guesses=guesses-1
         }
-        ]).then(function eachguess(answers) {
-            var usercharacter = answers.usercharacter
-            word.showcharacter(usercharacter)
-            if(chosenword.indexOf(usercharacter)==-1){
-                guesses=guesses-1
+        guessprompt()
+    })
+
+    function guessprompt(){
+        if(word.showstring().join("")===chosenword){
+            console.log("You are right! It was: "+chosenword.toUpperCase() + "\nVictory!!!")
+            inquirer.prompt([
+                { 
+                    type: "confirm",
+                    message: "Would you like another word?",
+                    name: "confirm",
+                    default: true
+                }
+            ])
+            .then(function(inquirerResponse) {
+                if (inquirerResponse.confirm) {
+                    newgame()
+                }
+                else{
+                    console.log("Congrats! It was a good game!")
+                }
+            })
+        }
+        else if(guesses>0){
+            inquirer.prompt([
+            {
+            type: "input",
+            name: "usercharacter",
+            message: "Keep guessing: "+word.showstring().join(" ") + "\nNumber of guesses: "+guesses+ "\nYour letter: ",
+            validate: function(value) {
+                if(word.showstring().indexOf(value)>-1){
+                    return "You already have this letter. Don't repeat!"
+                }
+                else if(triedletters.indexOf(value)>-1){
+                    return "You've already tried this one! Choose something else."
+                }
+                else if(value.match(/^[A-Za-z]+$/)&&value.length==1){
+                    return true
+                }
+                else {
+                    return "Please type a letter!"
+                }
             }
-            guessprompt()
-        })
-    }
-    else{
-        console.log("Gameover!")
+            }
+            ]).then(function eachguess(answers) {
+                var usercharacter = answers.usercharacter
+                triedletters.push(usercharacter)
+                word.showcharacter(usercharacter)
+                if(chosenword.indexOf(usercharacter)==-1){
+                    guesses=guesses-1
+                }
+                guessprompt()
+            })
+        }
+        else{
+            console.log("You are out of guesses!"+"\nIt was " +chosenword.toUpperCase())
+            inquirer.prompt([
+                { 
+                    type: "confirm",
+                    message: "Would you like another word?",
+                    name: "confirm",
+                    default: true
+                }
+            ])
+            .then(function(inquirerResponse) {
+                if (inquirerResponse.confirm) {
+                    newgame()
+                }
+                else{
+                    console.log("Gameover! It was a good game! Google your creature!")
+                }
+            })
+        }
     }
 }
+
+newgame()
